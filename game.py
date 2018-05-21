758be436f496f9505bf92ad7ce878364440875c8
Markiian <matsiuik@ucu.edu.ua>
2018-05-21 23:27:14 +0300

    check

--- game.py
+++ game.py
@@ -1,13 +1,39 @@
 board = """
-     |     |
-  1  |  2  |  3
+1    |2    |3
+ {} | {} | {}
 _____|_____|_____
-     |     |
-  4  |  5  |  6
+4    |5    |6
+ {} | {} | {}
 _____|_____|_____
-     |     |
-  7  |  8  |  9
+7    |8    |9
+ {} | {} | {}
      |     |
 """
 
-print(board)
+
+class Board():
+    def __init__(self):
+        self.board = [["   " for i in range(3)] for i in range(3)]
+
+    def __str__(self):
+        return board.format(*[item for sub in self.board for item in sub])
+
+    def check():
+        pos = [[(0, 0), (0, 1), (0, 2)],
+               [(1, 0), (1, 1), (1, 2)],
+               [(2, 0), (2, 1), (2, 2)],
+
+               [(0, 0), (1, 0), (2, 0)],
+               [(0, 1), (1, 1), (2, 1)],
+               [(0, 2), (1, 2), (2, 2)],
+
+               [(0, 0), (1, 1), (2, 2)],
+               [(0, 2), (1, 1), (2, 0)]]
+
+        for i in range(8):
+            pass
+
+
+x = Board()
+
+print(x)
