8f515a62c19e8b099ca90f8cf664d01154e54053
Markiian <matsiuik@ucu.edu.ua>
2018-05-21 23:51:34 +0300

    moooore logic

--- game.py
+++ game.py
@@ -11,9 +11,18 @@ _____|_____|_____
 """
 
 
+class OutOfFieldError(Exception):
+    pass
+
+
+class AlreadyOccupiedError(Exception):
+    pass
+
+
 class Board():
     def __init__(self):
         self.board = [["   " for i in range(3)] for i in range(3)]
+        self.previous_turn = None
 
     def __str__(self):
         return board.format(*[item for sub in self.board for item in sub])
@@ -38,17 +47,25 @@ class Board():
                     self.board[i[1][0]][i[1][1]] = "[X]"
                     self.board[i[2][0]][i[2][1]] = "[X]"
                     return -1
-                else:
+                elif self.board[i[0][0]][i[0][1]] == " O ":
                     self.board[i[0][0]][i[0][1]] = "[O]"
                     self.board[i[1][0]][i[1][1]] = "[O]"
                     self.board[i[2][0]][i[2][1]] = "[O]"
                     return 1
+                else:
+                    return 0
 
     def set(self, num, sign):
         variants = {1: (0, 0), 2: (0, 1), 3: (0, 2), 4: (1, 0), 5: (1, 1),
                     6: (1, 2), 7: (2, 0), 8: (2, 1), 9: (2, 2)}
         if num in variants:
-            self.board[variants[num][0]][variants[num][1]] = sign
+            if self.board[variants[num][0]][variants[num][1]] == "   ":
+                self.board[variants[num][0]][variants[num][1]] = sign
+                self.previous_turn = (sign, variants[num][1])
+            else:
+                raise AlreadyOccupiedError
+        else:
+            raise OutOfFieldError
 
 
 x = Board()
